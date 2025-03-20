1dacbdd3b6972d1905d6b9701f76f79553889b97
abdulkader33447 <abdulkader33447@gmail.com>
2025-03-20 13:56:22 +0600

    first commit

---
+++ consol.js
@@ -0,0 +1,16 @@
+let age = 26;
+console.log("age",age);
+
+const booksToRead = 6;
+console.log("book to rea",booksToRead);
+
+let wakeUpTime = 5;
+wakeUpTime = 4;
+console.log("wake up time",wakeUpTime);
+
+const wastedTime=3;
+console.log("wasted time today",wastedTime,"hours")
+
+let borrow = 0;
+console.log("amar kono dena nai",borrow)
+
---
+++ variable.js
@@ -0,0 +1,15 @@
+let weight = 52;
+
+const birthDate = 1999;
+
+const siblings = 5;
+
+const mark = 55;
+
+const rooms = 3;
+
+let ridingHours = 8;
+
+const total = 38;
+
+console.log(weight, birthDate, siblings, mark, rooms, ridingHours, total);
