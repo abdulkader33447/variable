597d0c64416ad23d8338aaddad41faf918454ff0
abdulkader33447 <abdulkader33447@gmail.com>
2025-03-20 14:03:30 +0600

    added two variable

--- consol.js
+++ consol.js
@@ -1,16 +1,21 @@
 let age = 26;
-console.log("age",age);
+console.log("age", age);
 
 const booksToRead = 6;
-console.log("book to rea",booksToRead);
+console.log("book to rea", booksToRead);
 
 let wakeUpTime = 5;
 wakeUpTime = 4;
-console.log("wake up time",wakeUpTime);
+console.log("wake up time", wakeUpTime);
 
-const wastedTime=3;
-console.log("wasted time today",wastedTime,"hours")
+const wastedTime = 3;
+console.log("wasted time today", wastedTime, "hours");
 
 let borrow = 0;
-console.log("amar kono dena nai",borrow)
+console.log("amar kono dena nai", borrow);
 
+const goals = 3;
+console.log("ei dol", goals, "ta diche");
+
+const pillow = 2;
+console.log("amar bichanay",pillow,"ta balish,amar ar vaier");
